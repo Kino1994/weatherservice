e0b9695341a073a3726d3d056544cb885a3a5db6
joaquin <jdevicente@genasys.com>
2021-01-10 02:03:17 +0100

    remove models

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let userSchema = new mongoose.Schema({
-    id: Number,
-    nick: String,
-    email: String,
-}, {versionKey: false});
-
-module.exports = mongoose.model('User', userSchema, 'users');
